588b9aed3b3e7cddd04eb4b2b1026c0cab48e86a
GuFFy12 <jokers_hello_0d@icloud.com>
2025-03-23 17:33:46 +0500

    bump release tag to v1.1.0

--- install.sh
+++ install.sh
@@ -2,7 +2,7 @@
 set -euo pipefail
 IFS=$'\n\t'
 
-RELEASE_TAG=v1.0.0
+RELEASE_TAG=v1.1.0
 
 ZAPRET_BASE="${ZAPRET_BASE:-/opt/zapret}"
 ZAPRET_SCRIPT="${ZAPRET_SCRIPT:-"$ZAPRET_BASE/init.d/sysv/zapret_keenetic.sh"}"
